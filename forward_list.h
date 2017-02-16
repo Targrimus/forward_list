5f9ab8033e1d85efef1fcf79f448899ab832a783
Rıfat Tolga KIRAN <tolga.kiran@hotmail.de>
2017-02-16 09:18:17 +0200

    Update forward_list.h

--- forward_list.h
+++ forward_list.h
@@ -1,95 +1,158 @@
 #ifndef FORWARD_LIST_H_
 #define FORWARD_LIST_H_
+#include <iostream>
 
+/*******************************
+ * forward_list implementation
+ *******************************/
 
-//	NODE implementation
-template <typename T>
-class forward_list_node{
-public:
-	using value_type = T;
-	using pointer_type = T*;
-	using reference_type = T&;
-
-	forward_list_node(value_type value = 0) :  data(value), next(nullptr) {
-	}
-
-	forward_list_node<value_type> * get_next() const{
-		return this->next;
-	}
-
-	void set_next(forward_list_node<value_type> * node){
-		this->next = node;
-	}
-
-	value_type get_data() const{
-		return this->data;
-	}
-
-	void set_data(value_type value){
-		this->data = value;
-	}
-
-private:
-	value_type data;
-	forward_list_node<value_type> * next;
-};
-
-
-//	FORWARD_list implementation
 template <typename T>
 class forward_list{
+
+	//	forward declaration of node class
+	class forward_list_node;
+
 public:
+	using node = forward_list_node;
+	using node_pointer = node*;
 	using value_type = T;
-	using pointer_type = T*;
-	using reference_type = T&;
+	using pointer	= T*;
+	using reference	= T&;
+	using counter_t	= size_t;
 
-	using node = forward_list_node<value_type>;
-	using node_pointer = node *;
+	//	default ctor
+	forward_list();
 
-	// ctor
-	forward_list() : head(nullptr), tail(nullptr){
-	}
+	//	copy ctor
+	//forward_list(const forward_list &);
+
+	// move ctor
+	//forward_list(forward_list &&);
 
 	//	dtor
-	~forward_list(){
-		node_pointer ptr = this->head, tmptr;
+	~forward_list();
 
-		while(ptr != nullptr){
-			tmptr = ptr;
-			std::cout << "GC: " << ptr->get_data() << " deleting..." << std::endl;
-			ptr = ptr->get_next();
-			delete tmptr;
-		}
+	//	copy assignment
+	//forward_list& operator=(const forward_list&);
 
-	}
+	//	move assignment
+	//forward_list& operator=(forward_list&&);
 
-	forward_list& push_back(value_type value){
-		node_pointer item = new node(value);
 
-		if(this->tail) this->tail->set_next(item);
-		this->tail = item;
-		if(!this->head) this->head = this->tail;
+	//	empty()
+	bool empty() const;
 
-		return *this;
-	}
+	// front() function
+	value_type front() const;
 
-	value_type pop_front(){
-		node_pointer item;
-		value_type data = 0;
+	// back() function
+	value_type back() const;
 
-		if(this->tail && this->head){
-			data = this->head->get_data();
-			item = this->head;
-			this->head = this->head->get_next();
-		}
+	//	push function
+	forward_list& push_back(value_type);
 
-		return data;
-	}
+	// pop_front() function
+	void pop_front();
+
 
 
 private:
-	node_pointer head, tail;
+	node_pointer _head, _tail;
+	counter_t _counter;
 };
+
+//	empty()
+template <typename T>
+bool forward_list<T>::empty() const{
+	return (this->_counter == 0);
+}
+
+//	default ctor
+template <typename T>
+forward_list<T>::forward_list() : _head(nullptr), _tail(nullptr), _counter(0){
+
+}
+
+template <typename T>
+forward_list<T>::~forward_list(){
+	node_pointer old_head;
+
+	while(this->_head != nullptr){
+		old_head = this->_head;
+		std::cout << "[GC] deleting: " << old_head->getData() << std::endl;
+		this->_head = this->_head->getNext();
+		delete old_head;
+	}
+}
+
+
+
+
+/******************************
+ *	NODE CLASS implementation
+ ******************************/
+
+template <typename T>
+class forward_list<T>::forward_list_node{
+public:
+	//	default ctor
+	forward_list_node();
+
+	//	value ctor
+	forward_list_node(value_type);
+
+	// setNext;
+	void setNext(node_pointer);
+
+	//	getNext;
+	node_pointer getNext() const;
+
+	// setData();
+	void setData(value_type);
+
+	//	getData();
+	value_type getData() const;
+
+private:
+	node_pointer _next;
+	value_type _data;
+};
+
+
+//	node default ctor;
+template <typename T>
+forward_list<T>::forward_list_node::forward_list_node() : _next(nullptr){
+}
+
+//	node value ctor
+template <typename T>
+forward_list<T>::forward_list_node::forward_list_node(value_type value) : _data(value), _next(nullptr){
+}
+
+//	node setNext()
+template <typename T>
+void forward_list<T>::forward_list_node::setNext(node_pointer next){
+	this->_next = next;
+}
+
+//	node getNext()
+template <typename T>
+typename forward_list<T>::node_pointer forward_list<T>::forward_list_node::getNext() const{
+	return this->_next;
+}
+
+//	node setData()
+template <typename T>
+void forward_list<T>::forward_list_node::setData(value_type value){
+	this->_data = value;
+}
+
+//	node getData()
+template <typename T>
+typename forward_list<T>::value_type forward_list<T>::forward_list_node::getData() const{
+	return this->_data;
+}
+
 
 
 
