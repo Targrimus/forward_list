675efa173fd7ebca5733cbf0e875f48dea7ac4ab
Rıfat Tolga KIRAN <tolga.kiran@hotmail.de>
2017-02-16 10:02:06 +0200

    Update forward_list.h
    added functions such as front(), back(), push_back(), pop_front();

--- forward_list.h
+++ forward_list.h
@@ -18,7 +18,8 @@ public:
 	using value_type = T;
 	using pointer	= T*;
 	using reference	= T&;
-	using counter_t	= size_t;
+	using const_reference = const T&;
+	using counter_t	= std::size_t;
 
 	//	default ctor
 	forward_list();
@@ -76,15 +77,62 @@ forward_list<T>::forward_list() : _head(nullptr), _tail(nullptr), _counter(0){
 template <typename T>
 forward_list<T>::~forward_list(){
 	node_pointer old_head;
-
 	while(this->_head != nullptr){
 		old_head = this->_head;
 		std::cout << "[GC] deleting: " << old_head->getData() << std::endl;
 		this->_head = this->_head->getNext();
 		delete old_head;
+		old_head = nullptr;
 	}
 }
 
+
+// front() function
+template <typename T>
+typename forward_list<T>::value_type forward_list<T>::front() const{
+	if(this->_head != nullptr)
+		return this->_head->getData();
+
+	return value_type();
+}
+
+// back() function
+template <typename T>
+typename forward_list<T>::value_type forward_list<T>::back() const{
+	if(this->_tail != nullptr)
+		return this->_tail->getData();
+
+	return value_type();
+}
+
+//	push function
+template <typename T>
+forward_list<T>& forward_list<T>::push_back(typename forward_list<T>::value_type value){
+	node_pointer item = new node(value);
+
+	if(this->_tail) this->_tail->setNext(item);
+	this->_tail = item;
+	++this->_counter;
+	if(!this->_head) this->_head = this->_tail;
+
+	return *this;
+}
+
+// pop_front() function
+template <typename T>
+void forward_list<T>::pop_front(){
+	if(this->_head && this->_tail){
+
+		node_pointer item;
+		value_type value;
+
+		value = this->_head->getData();
+		item = this->_head;
+		this->_head = this->_head->getNext();
+		delete item;
+		--this->_counter;
+	}
+}
 
 
 
@@ -126,7 +174,7 @@ forward_list<T>::forward_list_node::forward_list_node() : _next(nullptr){
 
 //	node value ctor
 template <typename T>
-forward_list<T>::forward_list_node::forward_list_node(value_type value) : _data(value), _next(nullptr){
+forward_list<T>::forward_list_node::forward_list_node(value_type value) : _next(nullptr), _data(value) {
 }
 
 //	node setNext()
