f4a7931535b1d074c4e5922063968203ae75833e
Rıfat Tolga KIRAN <tolga.kiran@hotmail.de>
2017-02-16 10:02:33 +0200

    Update forward_list.cpp
    testing

--- forward_list.cpp
+++ forward_list.cpp
@@ -5,7 +5,11 @@ int main(int argc, char ** argv)
 {
 	forward_list<int> numbers;
 
-
+	numbers.push_back(10).push_back(22).push_back(55);
+	std::cout << "is empty: " << std::boolalpha << numbers.empty() << std::endl;
+	numbers.pop_front();
+	std::cout << "front: " << numbers.front() << std::endl;
+	std::cout << "back: " << numbers.back() << std::endl;
 
 	return 0;
 }
